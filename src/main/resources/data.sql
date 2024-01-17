5d0061f219a8c14695e594d0d25bacc4b86786cd
Kishor7743 <balukura90@gmail.com>
2024-01-17 13:00:21 +0000

    solution

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO hotel (name, location, rating)
+VALUES ('The Plaza Hotel','New York', 4);
+
+INSERT INTO hotel (name, location, rating)
+VALUES ('The Beverly Hills Hotel', 'Los Angeles', 5);
+
+INSERT INTO hotel (name, location, rating)
+VALUES ('The Langham', 'Chicago', 3);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('A-101', 'Deluxe Room', 375.0, 1);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('A-205', 'Suite', 950.0, 1);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('B-106', 'Penthouse Suite', 2500.0, 1);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('C-401', 'Superior Guest Room', 465.0, 2);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('D-202', 'Bungalow', 1250.0, 2);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('A-107', 'Penthouse Suite', 3300.0, 2);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('A-301', 'Grand Room', 410.0, 3);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('C-313', 'Executive Suite', 700.0, 3);
+
+INSERT INTO room (roomNumber, type, price, id)
+VALUES ('D-404', 'Premier Suite', 880.0, 3);
